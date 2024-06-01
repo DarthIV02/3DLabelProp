0f595b0d98439305f99b612d9a116380effb69f4
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-05-31 21:05:32 -0700

    Update infer.py

--- infer.py
+++ infer.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         
         model_information.num_classes = train_set.get_n_label()
         model_information.ignore_label = -1
-        model_information.in_features_dim = model_cfg.architecture.in_features_dim
+        model_information.in_features_dim = model_cfg.architecture.n_features
         from models.kpconv_model import SemanticSegmentationModel
         module = importlib.import_module('models.kpconv.architecture')
         model_type = getattr(module, cfg.architecture.type)
