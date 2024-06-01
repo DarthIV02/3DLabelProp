d0d4538716663d27518e2aa661f23dd00711b3c8
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-06-01 00:03:18 -0700

    Debug

--- nautilus/3dlabelprop_setup.sh
+++ nautilus/3dlabelprop_setup.sh
@@ -1,18 +1,19 @@
-echo -y | apt-get install libmlpack-dev
-apt -y install python3-pybind11
-apt-get install liblapack-dev
-apt-get install libblas-dev
-apt-get install libarmadillo-dev
+echo Y | apt-get install libmlpack-dev
+echo Y | apt -y install python3-pybind11
+echo Y | apt-get install liblapack-dev
+echo Y | apt-get install libblas-dev
+echo Y | apt-get install libarmadillo-dev
 conda create --name 3DLabelProp python=3.7
 conda activate 3DLabelProp
 cd /home
 git clone https://github.com/DarthIV02/3DLabelProp.git
 cd 3DLabelProp/
-pip install -r requirements.txt
-echo -y | conda install pytorch==1.7.1 torchvision torchaudio cudatoolkit=11.0 -c pytorch
+echo Y | pip install -r requirements.txt
+echo Y | conda install pytorch==1.7.1 torchvision torchaudio cudatoolkit=11.0 -c pytorch
 pip install torch-sparse
 pip install pybind11
 cd cpp_wrappers
 bash compile_wrappers.sh
+#pip install --force-reinstall torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117
 
 # >kubectl cp --retries 10 semantickitti.zip label-prop-5c586cbd99-9snt6:/home/
