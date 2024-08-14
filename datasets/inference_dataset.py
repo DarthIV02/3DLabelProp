62fd70c2eed9feb6ac15636213b8dc8c38bb227b
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-08-13 20:04:31 -0700

    Debug

--- datasets/inference_dataset.py
+++ datasets/inference_dataset.py
@@ -37,6 +37,7 @@ def confmat_computations_parallel(frame_list,label_list=np.arange(-1,19),n_proce
     global compute_conf_mat
     def compute_conf_mat(frame):
         results = np.load(frame)
+        print(np.bincount(results))
         gt = target_mapping[results[:,-1].astype(np.int32)+1]
         pred = source_mapping[results[:,0].astype(np.int32)+1]
         c_mat = confusion_matrix(gt,pred,labels=label_list)
