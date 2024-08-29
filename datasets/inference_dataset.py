45b70a81f8c8b5b18e555b1f4238fc5b729d9188
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-08-29 10:21:26 -0700

    Change to save separately

--- datasets/inference_dataset.py
+++ datasets/inference_dataset.py
@@ -283,7 +283,8 @@ class InferenceDataset:
                     to_save = np.zeros((len(pointcloud),2),dtype=np.int32)
                     to_save[:,0] = accumulated_pointcloud[-len(pointcloud):,4].astype(np.int32)
                     to_save[:,-1] = label.astype(np.int32)
-                    np.save(osp.join(self.save, seq, str(frame)+'.npy'), to_save)
+                    print(self.save)
+                    np.save(osp.join(self.save, f'HD_{self.config.hd_block_stop}', seq, str(frame)+'.npy'), to_save)
 
                     st_real = True
                 
