301df41cb3f7ae8e48221df92f933654b24f4ae2
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-08-29 11:01:38 -0700

    Update inference_dataset.py

--- datasets/inference_dataset.py
+++ datasets/inference_dataset.py
@@ -281,6 +281,7 @@ class InferenceDataset:
                         comp_score[comp_score>self.config.cluster.override] = 1 
 
                         print(new_conf.shape)
+                        print("New Shape")
                         accumulated_pointcloud[-len(pointcloud):,4] = np.where(new_conf[-len(pointcloud):]>comp_score[-len(pointcloud):],acc_label[-len(pointcloud):],pred[-len(pointcloud):])
                         accumulated_confidence[-len(pointcloud):] = np.where(new_conf[-len(pointcloud):]>comp_score[-len(pointcloud):],new_conf[-len(pointcloud):],score[-len(pointcloud):])
 
