bff404d2b93bf0161e1825576c0f0ceaf7491bb7
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-08-29 11:35:02 -0700

    Update inference_dataset.py

--- datasets/inference_dataset.py
+++ datasets/inference_dataset.py
@@ -288,7 +288,7 @@ class InferenceDataset:
 
                     to_save = np.zeros((len(pointcloud),2),dtype=np.int32)
                     #to_save[:,0] = accumulated_pointcloud[-len(pointcloud):,4].astype(np.int32)
-                    to_save[:,0] = pred.astype(np.int32)
+                    to_save[:,0] = pred[-len(pointcloud):].astype(np.int32)
                     to_save[:,-1] = label.astype(np.int32)
                     #np.save(osp.join(self.save, f'HD_{self.config.hd_block_stop}', seq, str(frame)+'.npy'), to_save)
                     hdf_file.create_dataset(f'{str(frame)}', data=to_save)
