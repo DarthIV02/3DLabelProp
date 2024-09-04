601a10b4dead403a9df12ed365417ede59823f87
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-03 19:02:54 -0700

    Update inference_dataset.py

--- datasets/inference_dataset.py
+++ datasets/inference_dataset.py
@@ -92,7 +92,6 @@ def train_hd_concat_kp(model,clusters,device,config_model,batch_size,config_data
         if 'cuda' in device.type:
             r_clouds.to(device)
         model.model.train_hd(r_clouds, config_model, config_data, hd_model, label_whole) # This are the individual predictions
-        del label_out
         labels_here = []
         lengths = r_clouds.lengths[0].cpu().numpy()
         for i in range(len(sub_clusters)):
