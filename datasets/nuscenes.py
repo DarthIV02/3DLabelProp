0791f4a85c7a428c608f85d30913b9793336cb12
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-07 16:14:06 -0700

    Update nuscenes.py

--- datasets/nuscenes.py
+++ datasets/nuscenes.py
@@ -96,4 +96,5 @@ class nuScenes(PointCloudDataset):
     def get_poses_seq(self, seq_number):
         print("Self sequence")
         print(self.sequence)
+        print(self.sequence["scene-0034"])
         return read_transfo(osp.join(osp.join(self.path,self.traj_folder),self.sequence[seq_number]+'_traj_complete_result.txt'),False)
