710929917ca497eb83f5fe3e2075a69713e1ff36
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-07 16:18:50 -0700

    Update nuscenes.py

--- datasets/nuscenes.py
+++ datasets/nuscenes.py
@@ -94,4 +94,4 @@ class nuScenes(PointCloudDataset):
         return len(self.scenes[self.sequence[seq_number]]['label'])
 
     def get_poses_seq(self, seq_number):
-        return read_transfo(osp.join(osp.join(self.path,self.traj_folder),f'{seq_number}_traj_complete_result.txt'),False)
+        return read_transfo(osp.join(osp.join(self.path,self.traj_folder),f'{self.sequence[seq_number]}_traj_complete_result.txt'),False)
