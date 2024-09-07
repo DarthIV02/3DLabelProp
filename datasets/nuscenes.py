6a2f097a97e0d0fceef536a158b31235475a0152
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-07 16:23:17 -0700

    Update nuscenes.py

--- datasets/nuscenes.py
+++ datasets/nuscenes.py
@@ -91,7 +91,7 @@ class nuScenes(PointCloudDataset):
         return self.scenes[self.sequence[seq_number]]
 
     def get_size_seq(self, seq_number):
-        print(self.scenes)
+        print(self.scenes.keys())
         return len(self.scenes[self.sequence[seq_number]]['label'])
 
     def get_poses_seq(self, seq_number):
