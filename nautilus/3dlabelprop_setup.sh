54fcee5eb300a973d1a590dea979425b38bf2d77
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-15 16:56:45 -0700

    get dataset

--- nautilus/3dlabelprop_setup.sh
+++ nautilus/3dlabelprop_setup.sh
@@ -4,7 +4,7 @@ apt-get -y install liblapack-dev
 apt-get -y install libblas-dev
 apt-get -y install libarmadillo-dev
 apt-get -y install qt5-default
-#apt-get -y install cmake libopenblas-dev liblapack-dev libarpack2-de libsuperlu-dev libensmallen-dev
+apt-get -y install cmake libopenblas-dev liblapack-dev libarpack2-de libsuperlu-dev libensmallen-dev
 #apt-get -y install gcc-10.2 g++-10.2
 apt-get -y install libomp-dev
 apt-get -y install libcereal-dev libstb-dev
