15fe27380dc986d6df1c9ca06feda897b1a48ea9
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-15 17:07:31 -0700

    Update 3dlabelprop_setup.sh

--- nautilus/3dlabelprop_setup.sh
+++ nautilus/3dlabelprop_setup.sh
@@ -22,7 +22,7 @@ cd cpp_wrappers
 bash compile_wrappers.sh
 pip install --force-reinstall torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117
 conda install -c torchhd torchhd
-export LD_LIBRARY_PATH=/root/anaconda3/envs/3DLabelProp/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/root/anaconda3/envs/3DLabelProp/lib:$LD_LIBRARY_PATH
 # OR export LD_LIBRARY_PATH=/home/ubuntu/anaconda3/envs/3DLabelProp/lib:$LD_LIBRARY_PATH
 
 
