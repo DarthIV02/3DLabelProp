fa296286cb966457f8f1502fbcaaa45db939c262
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-17 15:35:40 -0700

    Change neighbors

--- models/kpconv_model.py
+++ models/kpconv_model.py
@@ -133,7 +133,7 @@ class SemanticSegmentationModel:
         self.config = config
         self.helper_function = PointCloudDataset('token', self.model_config)
         self.helper_function.config = self.model_config
-        self.path = join(self.config.cluster.path,self.config.source,self.config.cluster.name)
+        self.path = join(self.config.cluster.path, self.config.cluster.name)
         #print("HERRRRREEE")
         #print(self.config.cluster.path)
         self.batch_limit = torch.tensor([1], dtype=torch.float32)
