3943995ae2914e33d1a24b8d240e054928c2b200
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-10-03 21:17:17 -0700

    Change

--- datasets/inference_dataset.py
+++ datasets/inference_dataset.py
@@ -206,7 +206,6 @@ class InferenceDataset:
         return ius, miu
 
     def compute_dataset(self):
-        print("Sequence: ", self.trg_datast.sequence)
         for i in tqdm(range(len(self.trg_datast.sequence)),desc="Processing dataset "+str(self.config.target)):
             self.compute_sequence(i)
 
