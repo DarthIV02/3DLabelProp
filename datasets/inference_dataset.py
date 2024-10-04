6db397d7a27d887f84cce237d4d1fb4d0e41ed10
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-10-03 22:10:18 -0700

    Update inference_dataset.py

--- datasets/inference_dataset.py
+++ datasets/inference_dataset.py
@@ -444,7 +444,9 @@ class InferenceDataset:
                     accumulated_confidence = accumulated_confidence[norm_acc<self.config.sequence.limit_GT]
 
                 accumulated_pointcloud = np.vstack((accumulated_pointcloud,pointcloud))
+                print("Pc:", accumulated_pointcloud.shape)
                 accumulated_confidence = np.concatenate((accumulated_confidence,np.zeros(len(pointcloud))))
+                print("Conf:", accumulated_confidence.shape)
 
                 acc_label = np.copy(accumulated_pointcloud[:,4].astype(np.int32))
                 acc_label, new_conf = compute_labels(accumulated_pointcloud, acc_label, accumulated_confidence, len(pointcloud), self.config.sequence.voxel_size, self.n_label, self.config.source, self.config.sequence.dist_prop)
